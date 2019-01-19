d327b80586e5b3a3d4b2e363b6222ab8f7b3247f
Ben Sorohan <ben@backbase.com>
2019-01-19 21:35:28 +0100

    Add a public API.

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+export { GraphBuilder } from './graph/GraphBuilder';
+export { ValueGraphBuilder } from './graph/ValueGraphBuilder';
+export { MutableGraph } from './graph/MutableGraph';
+export { MutableValueGraph } from './graph/MutableValueGraph';
+export { ValueGraph } from './graph/ValueGraph';
+export { Graph } from './graph/Graph';
+export { GraphConnections } from './graph/GraphConnections';
+export { BaseGraph } from './graph/BaseGraph';
+export { SuccessorsFunction } from './graph/SuccessorsFunction';
+export { PredecessorsFunction } from './graph/PredecessorsFunction';
+export { EndpointPair } from './graph/EndpointPair';
+export { ElementOrder } from './graph/ElementOrder';
