7892f36b8e7b80d6afdbff417c8298bf7ff821f7
Ben Sorohan <ben@backbase.com>
2019-01-26 14:44:33 +0100

    Simplify the tests for traversal

--- src/graph/Traverser.spec.ts
+++ src/graph/Traverser.spec.ts
@@ -4,102 +4,81 @@ import { GraphBuilder } from './GraphBuilder';
 
 // Graphs
 
-test('Traverser :: graph :: depth first preorder :: single node', (t) => {
-  const g = GraphBuilder.directed<number>().allowsSelfLoops(true).build();
-  g.addNode(1);
-  const i: Iterable<number> = Traverser.forGraph(g).depthFirstPreOrder(1);
-  t.deepEquals(Array.from(i), [1]);
-  t.end();
-});
+/**
+ * test graph (numbered breadth first)
+ * 1
+ * | \
+ * 3  4  2
+ *     \ |
+ *       5
+ */
 
-test('Traverser :: graph :: depth first preorder :: two nodes', (t) => {
+const makeGraph = () => {
   const g = GraphBuilder.directed<number>().allowsSelfLoops(true).build();
-  g.putEdge(1, 2);
-  const i: Iterable<number> = Traverser.forGraph(g).depthFirstPreOrder(1);
-  t.deepEquals(Array.from(i), [1, 2]);
-  t.end();
-});
-
-test('Traverser :: graph :: depth first postorder :: single node', (t) => {
-  const g = GraphBuilder.directed<number>().allowsSelfLoops(true).build();
-  g.addNode(1);
-  const i: Iterable<number> = Traverser.forGraph(g).depthFirstPostOrder(1);
-  t.deepEquals(Array.from(i), [1]);
-  t.end();
-});
-
-test('Traverser :: graph :: depth first postorder :: two nodes', (t) => {
-  const g = GraphBuilder.directed<number>().allowsSelfLoops(true).build();
-  g.putEdge(1, 2);
-  const i: Iterable<number> = Traverser.forGraph(g).depthFirstPostOrder(1);
-  t.deepEquals(Array.from(i), [2, 1]);
-  t.end();
-});
-
-test('Traverser :: graph :: breadth first', (t) => {
-  const g = GraphBuilder.directed<number>().allowsSelfLoops(true).build();
-  /**
-   * 1       2
-   * | \     |
-   * 3  4----5
-   */
   g.putEdge(1, 3);
   g.putEdge(1, 4);
   g.putEdge(2, 5);
   g.putEdge(4, 5);
+  return g;
+};
+
+test('Traverser :: graph :: breadth first', (t) => {
+  const g = makeGraph();
   const i: Iterable<number> = Traverser.forGraph(g).breadthFirstN([1, 2]);
   t.deepEquals(Array.from(i), [1, 2, 3, 4, 5]);
   t.end();
 });
 
+test('Traverser :: graph :: depth first preorder', (t) => {
+  const g = makeGraph();
+  const i: Iterable<number> = Traverser.forGraph(g).depthFirstPreOrderN([1, 2]);
+  t.deepEquals(Array.from(i), [1, 3, 4, 5, 2]);
+  t.end();
+});
+
+test('Traverser :: graph :: depth first postorder', (t) => {
+  const g = makeGraph();
+  const i: Iterable<number> = Traverser.forGraph(g).depthFirstPostOrderN([1, 2]);
+  t.deepEquals(Array.from(i), [3, 5, 4, 1, 2]);
+  t.end();
+});
+
 // Trees
 
-test('Traverser :: tree :: depth first preorder :: single node', (t) => {
-  const tree = GraphBuilder.directed<number>().allowsSelfLoops(true).build();
-  tree.addNode(1);
-  const i: Iterable<number> = Traverser.forTree(tree).depthFirstPreOrder(1);
-  t.deepEquals(Array.from(i), [1]);
-  t.end();
-});
-
-test('Traverser :: tree :: depth first preorder :: two nodes', (t) => {
-  const tree = GraphBuilder.directed<number>().allowsSelfLoops(true).build();
-  tree.putEdge(1, 2);
-  const i: Iterable<number> = Traverser.forTree(tree).depthFirstPreOrder(1);
-  t.deepEquals(Array.from(i), [1, 2]);
-  t.end();
-});
-
-test('Traverser :: tree :: depth first postorder :: single node', (t) => {
-  const tree = GraphBuilder.directed<number>().allowsSelfLoops(true).build();
-  tree.addNode(1);
-  const i: Iterable<number> = Traverser.forTree(tree).depthFirstPostOrder(1);
-  t.deepEquals(Array.from(i), [1]);
-  t.end();
-});
-
-test('Traverser :: tree :: depth first postorder :: two nodes', (t) => {
+/**
+ *
+ * test tree (numbered in breadth first order)
+ * 1
+ * | \
+ * 2  3
+ * |
+ * 4
+ */
+const makeTree = () => {
   const tree = GraphBuilder.directed<number>().allowsSelfLoops(true).build();
   tree.putEdge(1, 2);
-  const i: Iterable<number> = Traverser.forTree(tree).depthFirstPostOrder(1);
-  t.deepEquals(Array.from(i), [2, 1]);
-  t.end();
-});
+  tree.putEdge(1, 3);
+  tree.putEdge(2, 4);
+  return tree;
+};
 
 test('Traverser :: tree :: breadth first', (t) => {
-  const tree = GraphBuilder.directed<number>().allowsSelfLoops(true).build();
-  /**
-   * 1
-   * | \
-   * 2  3
-   * |
-   * 4
-   */
-  tree.putEdge(1, 3);
-  tree.putEdge(1, 4);
-  tree.putEdge(2, 5);
-  tree.putEdge(4, 5);
-  const i: Iterable<number> = Traverser.forGraph(tree).breadthFirstN([1, 2]);
-  t.deepEquals(Array.from(i), [1, 2, 3, 4, 5]);
+  const tree = makeTree();
+  const i: Iterable<number> = Traverser.forTree(tree).breadthFirst(1);
+  t.deepEquals(Array.from(i), [1, 2, 3, 4]);
+  t.end();
+});
+
+test('Traverser :: tree :: depth first preorder', (t) => {
+  const tree = makeTree();
+  const i: Iterable<number> = Traverser.forTree(tree).depthFirstPreOrder(1);
+  t.deepEquals(Array.from(i), [1, 2, 4, 3]);
+  t.end();
+});
+
+test('Traverser :: tree :: depth first postorder', (t) => {
+  const tree = makeTree();
+  const i: Iterable<number> = Traverser.forTree(tree).depthFirstPostOrder(1);
+  t.deepEquals(Array.from(i), [4, 2, 3, 1]);
   t.end();
 });
