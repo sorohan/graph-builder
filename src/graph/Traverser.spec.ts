9d30f787008ab04d80a5186e84aa7fc1e229fb24
Ben Sorohan <ben@backbase.com>
2019-01-26 13:55:14 +0100

    Add more simple tests for 1 or 2 nodes, for depth first (pre and post order).

--- src/graph/Traverser.spec.ts
+++ src/graph/Traverser.spec.ts
@@ -9,3 +9,31 @@ test('Traverser :: graph :: depth first preorder :: single node', (t) => {
   t.deepEquals(Array.from(i), [1]);
   t.end();
 });
+
+test('Traverser :: graph :: depth first preorder :: two nodes', (t) => {
+  const g = GraphBuilder.directed<number>().allowsSelfLoops(true).build();
+  g.addNode(1);
+  g.addNode(2);
+  g.putEdge(1, 2);
+  const i: Iterable<number> = Traverser.forGraph(g).depthFirstPreOrder(1);
+  t.deepEquals(Array.from(i), [1, 2]);
+  t.end();
+});
+
+test('Traverser :: graph :: depth first postorder :: single node', (t) => {
+  const g = GraphBuilder.directed<number>().allowsSelfLoops(true).build();
+  g.addNode(1);
+  const i: Iterable<number> = Traverser.forGraph(g).depthFirstPostOrder(1);
+  t.deepEquals(Array.from(i), [1]);
+  t.end();
+});
+
+test('Traverser :: graph :: depth first postorder :: two nodes', (t) => {
+  const g = GraphBuilder.directed<number>().allowsSelfLoops(true).build();
+  g.addNode(1);
+  g.addNode(2);
+  g.putEdge(1, 2);
+  const i: Iterable<number> = Traverser.forGraph(g).depthFirstPostOrder(1);
+  t.deepEquals(Array.from(i), [2, 1]);
+  t.end();
+});
